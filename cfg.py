80c4010cc39a6f3f831db5c65d7ed5fc1dc1c8e3
Sudharshan <72936645+sudharshan2001@users.noreply.github.com>
2022-07-28 18:24:30 +0530

    Add files via upload

---
+++ cfg.py
@@ -0,0 +1,9 @@
+class CFG:
+    num_classes = 6
+    width = 224
+    height = 224
+    channel = 3
+    batch_size=32
+    base_dir='./intel-image-classification/seg_train/seg_train/*')
+    EPOCHS = 30
+    lr = 0.001
